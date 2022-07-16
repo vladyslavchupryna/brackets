769d731840764d35a0ad5278ade2bc59650ee7e6
Vladyslav Chupryna <vladyslavchuprynadev@gmail.com>
2022-07-16 21:28:49 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 === 1) {
+		return false;
+	}
+
+	const pairs = bracketsConfig.reduce((obj, curr) => {
+		obj[curr[0]] = curr[1];
+		return obj;
+	}, {});
+
+	let stack = [];
+
+	for (let i = 0; i < str.length; i++) {
+		let bracket = str[i];
+
+		if (bracket in pairs) {
+			if (bracket === pairs[bracket] && bracket === stack[stack.length - 1]) {
+				stack.pop();
+			} else {
+				stack.push(bracket);
+			}
+		} else {
+			if (stack.length === 0) {
+				return false;
+			}
+
+			if (bracket === pairs[stack[stack.length - 1]]) {
+				stack.pop();
+			}
+		}
+	}
+	return !stack.length;
 }
